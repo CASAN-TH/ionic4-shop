22b4b7ecc4cac25af2263bb16da713731d96ed14
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2019-11-21 06:52:09 +0700

    update & fix issue categiry page

--- src/app/category/category.page.ts
+++ src/app/category/category.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CategoryService } from './category.service';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
 
 @Component({
@@ -9,9 +10,9 @@ import { CategoryService } from './category.service';
   styleUrls: ['./category.page.scss'],
 })
 export class CategoryPage implements OnInit {
-  // @ViewChild("Content") content: Content;
+  @ViewChild('content', { static: false }) private content: any;
   data: any;
-
+  tabSelected: any;
   constructor(private router: Router, private categoryService: CategoryService) { }
 
 
@@ -20,16 +21,33 @@ export class CategoryPage implements OnInit {
     this.categoryService.onCategoryDataListChanged.subscribe((categoryDataList: any) => {
       console.log(categoryDataList);
       this.data = categoryDataList;
+      if (this.data.cate_data) {
+        this.tabSelected = this.data.cate_data[0]._id;
+      }
+
     })
+  }
+
+  onMenuClick(cate_id) {
+
+    this.tabSelected = cate_id;
+    let yOffset = document.getElementById(cate_id).offsetTop;
+    // let yHOffset = document.getElementById(cate_id).offsetHeight
+    // if (yOffset > 0) {
+    //   yOffset = yOffset + yHOffset;
+    // }
+    console.log(yOffset);
+    this.content.scrollToPoint(0, yOffset, 1000);
+    // this.content.scrollToBottom();
+    // this.content.scrollTo(yOffset)
   }
 
   onBrandClick(categoryId: any, brandId: any) {
     this.router.navigateByUrl('search/' + categoryId + '/' + brandId);
   }
 
-  // scrollTo(element:string) {
-  //   let yOffset = document.getElementById("14423").offsetTop;
-  //   this.content.scrollTo(0, yOffset, 4000)
-  // }
+  scrollTo(element: string) {
+
+  }
 
 }
