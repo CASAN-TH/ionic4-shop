43173f197ff8df4935e82e4683fb8660edebc973
Natsulelou <nutshapon_lelou@hotmail.com>
2020-01-31 17:51:56 +0700

    checkout

--- src/app/casan/checkout/checkout-type/checkout-type.component.ts
+++ src/app/casan/checkout/checkout-type/checkout-type.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActionSheetController, ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { SelectdownModalComponent } from 'src/app/payment/selectdown-modal/selectdown-modal.component';
 
 @Component({
   selector: 'app-checkout-type',
@@ -7,8 +10,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutTypeComponent implements OnInit {
 
-  constructor() { }
+  @Input() checkoutType: any;
+  @Input() cartDataList: any;
+  @Input() creditPoint: any;
+  @Input() profilestatus: any;
+  @Input() downDataList: any;
+  @Input() selectdownData: any;
 
-  ngOnInit() {}
+  unitSelected: any;
+  AcceptClick: boolean = false;
+  constructor(
+    public actionSheetController: ActionSheetController,
+    private router: Router,
+    private modalController: ModalController
+  ) { }
+
+  ngOnInit() { }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'วิธีการชำระเงิน',
+      buttons: [{
+        text: 'ผ่อนชำระ',
+        handler: () => {
+          this.checkoutType = "ผ่อนชำระ"
+        }
+      }, {
+        text: 'ชำระเต็มจำนวนเงิน',
+        handler: () => {
+          this.checkoutType = "ชำระเต็มจำนวนเงิน"
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  async actionPayment() {
+    if (this.cartDataList.totalcart.total_discount > this.creditPoint.credit.creditremain) {
+      const actionSheet = await this.actionSheetController.create({
+        header: 'วิธีการชำระเงิน',
+        buttons: [{
+          text: 'ชำระเต็มจำนวนเงิน',
+          handler: () => {
+            this.checkoutType = "ชำระเต็มจำนวนเงิน"
+          }
+        }]
+      });
+      await actionSheet.present();
+    } else {
+      const actionSheet = await this.actionSheetController.create({
+        header: 'วิธีการชำระเงิน',
+        buttons: [{
+          text: 'ผ่อนชำระ',
+          handler: () => {
+            this.checkoutType = "ผ่อนชำระ"
+          }
+        }, {
+          text: 'ชำระเต็มจำนวนเงิน',
+          handler: () => {
+            this.checkoutType = "ชำระเต็มจำนวนเงิน"
+          }
+        }]
+      });
+      await actionSheet.present();
+    }
+  }
+
+  onApplyForCredit() {
+    this.router.navigateByUrl('credit');
+  }
+
+  async selectDownModal() {
+    const modal = await this.modalController.create({
+      component: SelectdownModalComponent,
+      cssClass: 'my-modal-css',
+      componentProps: {
+        selectdownData: this.selectdownData
+      }
+    });
+    return await modal.present();
+  }
+  
+  onUnitClick(unitId: any) {
+    // this.router.navigateByUrl('search/' + cate2Id);
+    console.log(unitId);
+    this.unitSelected = unitId;
+  }
 
 }
