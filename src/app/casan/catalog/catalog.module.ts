cdea1782a5d97f8583bc71a4dc79156a8201550f
thanayutPhi <thanayut.phi@outlook.com>
2020-02-10 20:22:04 +0700

    edit work

--- src/app/casan/catalog/catalog.module.ts
+++ src/app/casan/catalog/catalog.module.ts
@@ -1,23 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductListBlockComponent } from './product-list-block/product-list-block.component';
+// import { ProductListComponent } from './product-list/product-list.component';
+// import { ProductListBlockComponent } from './product-list-block/product-list-block.component';
 import { IonicModule } from '@ionic/angular';
-import { ProductCardComponent } from './product-card/product-card.component';
+// import { ProductCardComponent } from './product-card/product-card.component';
 import { CatalogSlideNavComponent } from './catalog-slide-nav/catalog-slide-nav.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { ProductGeneralReccommentComponent } from './product-general-reccomment/product-general-reccomment.component';
 import { CataProductListComponent } from './cata-product-list/cata-product-list.component';
 import { MenuGeneralSelectCheckComponent } from './menu-general-select-check/menu-general-select-check.component';
 import { MenuSelectCheckComponent } from './menu-select-check/menu-select-check.component';
+// import { ProductListComponent } from './cata-product-list/product-list/product-list.component';
+import { ProductListBlockComponent } from './cata-product-list/product-list-block/product-list-block.component';
+import { ProductCardComponent } from './cata-product-list/product-card/product-card.component';
+import { ProductListComponent } from './product-general-reccomment/product-list/product-list.component';
 
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
+    // ProductListComponent,
     ProductListBlockComponent,
     ProductCardComponent,
+    // ProductListComponent,
+    // ProductListBlockComponent,
+    // ProductCardComponent,
     CatalogSlideNavComponent,
     CategoriesComponent,
     ProductGeneralReccommentComponent,
@@ -32,6 +40,8 @@ import { MenuSelectCheckComponent } from './menu-select-check/menu-select-check.
   exports: [
     ProductListComponent,
     ProductListBlockComponent,
+    // ProductListComponent,
+    // ProductListBlockComponent,
     CatalogSlideNavComponent,
     CategoriesComponent,
     ProductGeneralReccommentComponent,
